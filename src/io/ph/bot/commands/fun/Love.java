842d1aa3c53462fdd11880e9b0ad071716e8611f
nickalaskreynolds <nickalaskreynolds@gmail.com>
2017-07-27 04:57:36 +0000

    Initial commit

---
+++ src/io/ph/bot/commands/fun/Love.java
@@ -0,0 +1,27 @@
+package io.ph.bot.commands.fun;
+
+import io.ph.bot.commands.Command;
+import io.ph.bot.commands.CommandCategory;
+import io.ph.bot.commands.CommandData;
+import io.ph.bot.model.Permission;
+import io.ph.util.Util;
+import net.dv8tion.jda.core.entities.Message;
+
+@CommandData (
+                defaultSyntax = "love",
+                aliases = {"hug", "glomp"},
+                category = CommandCategory.FUN,
+                permission = Permission.NONE,
+                description = "Give love to another user.",
+                example = "Jiminya Oppa loved Mirrorsound!"
+                )
+
+public class Love extends Command {
+    @Override
+    public void executeCommand(Message msg) {
+                String lover = msg.getAuthor().getName();
+                String loved = msg.getMentionedUsers().size() > 0 ? msg.getMentionedUsers().get(0).getName();
+                String loved = Util.getCommandContents(msg);
+                msg.getChannel().sendMessage(lover + " gave love to " + loved).queue();
+    }
+}
