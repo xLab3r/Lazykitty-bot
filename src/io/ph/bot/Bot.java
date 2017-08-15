724574ef44df023566d352d3e82c5990ad97a7ae
nickalaskreynolds <nickalaskreynolds@gmail.com>
2017-08-15 02:04:36 +0000

    iterating version

--- src/io/ph/bot/Bot.java
+++ src/io/ph/bot/Bot.java
@@ -53,7 +53,7 @@ public class Bot {
 
 	// Set to true if you want various debug statements
 	public static final boolean DEBUG = false;
-	public static final String BOT_VERSION = "1.3beta";
+	public static final String BOT_VERSION = "1.4";
 	public static boolean isReady = false;
 
 	private APIKeys apiKeys = new APIKeys();
